7fadd26cac4e04025d853c982f2387c1f8ddb1d9
tama <tama@ttk1.net>
2019-08-05 02:07:58 +0900

    vec2クラスとparticleクラス作成

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import { Particle } from './particle';
+import { Vec2 } from './vec2';
+
 window.onload = (): void => {
     const container = document.getElementById('container');
     if (container) {
-        container.appendChild(createElement('h1', 'hello, world', null));
-        container.appendChild(createElement('p', dummyText(100), null));
+        main(container);
     }
 };
+
+function main(container: HTMLElement) {
+    container.appendChild(createElement('h1', 'hello, world', null));
+    container.appendChild(createElement('p', dummyText(100), null));
+    const p: Vec2 = new Vec2(0.0, 0.0);
+    const v: Vec2 = new Vec2(0.0, 0.0);
+    const particle = new Particle(p, v);
+}
 
 function createElement(tagName: string, innerHTML: string, child: HTMLProgressElement | null): HTMLElement {
     const element = document.createElement(tagName);
@@ -26,3 +36,4 @@ function dummyText(length: number): string {
 function randomChar(): string {
     return 'a';
 }
+
---
+++ src/particle.ts
@@ -0,0 +1,11 @@
+import { Vec2 } from './vec2';
+
+export class Particle {
+    public position: Vec2;
+    public velocity: Vec2;
+
+    constructor(position: Vec2, velocity: Vec2) {
+        this.position = position;
+        this.velocity = velocity;
+    }
+}
